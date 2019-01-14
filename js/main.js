3ab815e88550032a4fe476537a88f1b00c38c372
Legrand29 <lake.frantz@gmail.com>
2019-01-14 12:52:59 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Animation Smooth Scroll
+$('#view-work').on('click', function(){
+    const images = $('#images').position().top;
+
+    $('html, body').animate(
+        {
+            scrollTop: images
+        },
+        900
+    );
+});
